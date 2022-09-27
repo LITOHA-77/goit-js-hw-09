a016d3d6affe786a52db4200b037b20a7e085e12
Maksym Lytovchenko <lmb1988@ukr.net>
2022-09-27 12:38:48 +0300

    1 and 2 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.start.addEventListener('click', fonChangeStart);
+refs.stop.addEventListener('click', fonChangeStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onFonChange() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function fonChangeStart() {
+  id = setInterval(() => {
+    onFonChange();
+  }, 1000);
+  refs.start.disabled = true;
+}
+
+function fonChangeStop() {
+  clearInterval(id);
+  refs.start.disabled = false;
+}
